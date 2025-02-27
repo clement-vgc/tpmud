bbaa206b7b523b947e3d04eeb4316bb1bc35f907
clement-vgc <clement.vignon-chaudey@etu.univ-orleans.fr>
2025-02-27 10:45:04 +0100

    modified:   Box.java 	modified:   TestsBoxes.java

--- Box.java
+++ Box.java
@@ -1,17 +1,74 @@
 import java.util.ArrayList;
 import java.util.List;
-class Thing{
-String name;
-public Thing(String name){
-this.name = name;
+
+class Thing {
+    String name;
+    
+    public Thing(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Thing thing = (Thing) obj;
+        return name.equals(thing.name);
+    }
+    
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
-}
-class Box{
-List<Thing> contents;
-public Box(){
-    this.contents = new ArrayList<Thing>();
-}
-public void add(Thing truc){
-    this.contents.add(truc);
-}
+
+class Box {
+    private List<Thing> contents;
+    private boolean isOpen;
+    
+    public Box() {
+        this.contents = new ArrayList<>();
+        this.isOpen = true;
+    }
+    
+    public void add(Thing truc) {
+        this.contents.add(truc);
+    }
+    
+    public boolean contains(Thing truc) {
+        return this.contents.contains(truc);
+    }
+    
+    public void remove(Thing truc) throws Exception {
+        if (!this.contents.contains(truc)) {
+            throw new Exception("L'élément n'est pas dans la boîte.");
+        }
+        this.contents.remove(truc);
+    }
+    
+    public boolean isOpen() {
+        return this.isOpen;
+    }
+    
+    public void open() {
+        this.isOpen = true;
+    }
+    
+    public void close() {
+        this.isOpen = false;
+    }
+    
+    public String actionLook() {
+        if (!this.isOpen) {
+            return "La boîte est fermée.";
+        }
+        if (this.contents.isEmpty()) {
+            return "La boîte est vide.";
+        }
+        StringBuilder result = new StringBuilder("La boîte contient: ");
+        for (Thing t : contents) {
+            result.append(t.name).append(", ");
+        }
+        return result.substring(0, result.length() - 2);
+    }
 }
--- TestsBoxes.java
+++ TestsBoxes.java
@@ -5,7 +5,6 @@ public class TestsBoxes{
     public void testBoxCreate() {
         Box b = new Box();
     }
-    /** on veut pouvoir mettre des trucs dedans */
     @Test
     public void testBoxAdd(){
     Box b = new Box();
@@ -13,5 +12,46 @@ public class TestsBoxes{
     Thing truc2 = new Thing("truc2");
     b.add(truc1);
     b.add(truc2);
+    assertTrue(b.contains(truc1));
+    assertTrue(b.contains(truc2));
     }
+    @Test
+    public void testBoxRemove() throws Exception {
+        Box b = new Box();
+        Thing truc = new Thing("truc");
+        b.add(truc);
+        b.remove(truc);
+        assertFalse(b.contains(truc));
+    }
+    
+    @Test(expected = Exception.class)
+    public void testBoxRemoveNonExistent() throws Exception {
+        Box b = new Box();
+        Thing truc = new Thing("truc");
+        b.remove(truc);
+    }
+    
+    @Test
+    public void testBoxOpenClose() {
+        Box b = new Box();
+        b.close();
+        assertFalse(b.isOpen());
+        b.open();
+        assertTrue(b.isOpen());
+    }
+    
+    @Test
+    public void testActionLook() {
+        Box b = new Box();
+        assertEquals("La boîte est vide.", b.actionLook());
+        Thing truc1 = new Thing("ceci");
+        Thing truc2 = new Thing("cela");
+        b.add(truc1);
+        b.add(truc2);
+        assertEquals("La boîte contient: ceci, cela", b.actionLook());
+        b.close();
+        assertEquals("La boîte est fermée.", b.actionLook());
+    }
+}
+
 }
