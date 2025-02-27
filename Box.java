44771e2bb05ba3cf57ee1d8443e3c63eb3781597
clement-vgc <clement.vignon-chaudey@etu.univ-orleans.fr>
2025-02-27 10:04:35 +0100

    modified:   Box.java

--- Box.java
+++ Box.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+class Thing{
+String name;
+public Thing(String name){
+this.name = name;
+}
+}
 class Box{
-    ArrayList<String> contents = new ArrayList<String>();
-    public void add(String truc) {
-    this.contents.add(truc);
-    }
+ArrayList<Thing> contents = new ArrayList<Thing>();
+public void add(Thing truc):
+this.contents.add(truc);
 }
