8af8653d2ebb66fa7a5dc9cbe1ea82d01c1ddb31
clement-vgc <clement.vignon-chaudey@etu.univ-orleans.fr>
2025-02-27 10:51:48 +0100

    modified:   Box.class 	modified:   TestsBoxes.class 	modified:   TestsBoxes.java 	modified:   Thing.class

--- TestsBoxes.java
+++ TestsBoxes.java
@@ -1,45 +1,51 @@
 import org.junit.*;
-import static org.junit.Assert.assertEquals;
-public class TestsBoxes{
+import static org.junit.Assert.*;
+
+public class TestsBoxes {
+
     @Test
     public void testBoxCreate() {
         Box b = new Box();
+        assertEquals(true, b.isOpen()); // Vérifie si la boîte est ouverte après création
+        assertEquals("La boîte est vide.", b.actionLook());
     }
+
     @Test
-    public void testBoxAdd(){
-    Box b = new Box();
-    Thing truc1 = new Thing("truc1");
-    Thing truc2 = new Thing("truc2");
-    b.add(truc1);
-    b.add(truc2);
-    assertTrue(b.contains(truc1));
-    assertTrue(b.contains(truc2));
+    public void testBoxAdd() {
+        Box b = new Box();
+        Thing truc1 = new Thing("truc1");
+        Thing truc2 = new Thing("truc2");
+        b.add(truc1);
+        b.add(truc2);
+        assertEquals(true, b.contains(truc1));  // Utilisation de contains()
+        assertEquals(true, b.contains(truc2));
     }
+
     @Test
     public void testBoxRemove() throws Exception {
         Box b = new Box();
         Thing truc = new Thing("truc");
         b.add(truc);
         b.remove(truc);
-        assertFalse(b.contains(truc));
+        assertEquals(false, b.contains(truc));  // Utilisation de contains()
     }
-    
+
     @Test(expected = Exception.class)
     public void testBoxRemoveNonExistent() throws Exception {
         Box b = new Box();
         Thing truc = new Thing("truc");
         b.remove(truc);
     }
-    
+
     @Test
     public void testBoxOpenClose() {
         Box b = new Box();
         b.close();
-        assertFalse(b.isOpen());
+        assertEquals(false, b.isOpen());
         b.open();
-        assertTrue(b.isOpen());
+        assertEquals(true, b.isOpen());
     }
-    
+
     @Test
     public void testActionLook() {
         Box b = new Box();
